a0880b2a07611a206621e92e5b5f42dd027038bd
Volodymyr <gutmann003@gmail.com>
2022-03-30 15:20:07 +0300

    second problem

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,8 +1,8 @@
 module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
-    EMAIL: psrocess.env.EMAIL,
-    EMAIL_PASSWORD: psrocess.env.EMAIL_PASSWORD,
-    EMAIL_FROM: psrocess.env.EMAIL_FROM,
-    BASE_URL: psrocess.env.BASE_URL
+    EMAIL: process.env.EMAIL,
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    BASE_URL: process.env.BASE_URL
 }
