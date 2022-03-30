bb255869f03208d9fdf8b6b6d13f0d3ae82a17cb
Volodymyr <gutmann003@gmail.com>
2022-03-30 15:43:41 +0300

    disconect keys prod

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,14 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./keys.prod')
-}else{
-    module.exports = require('./keys.dev')
+// if(process.env.NODE_ENV === 'production'){
+//     module.exports = require('./keys.prod')
+// }else{
+//     module.exports = require('./keys.dev')
+// }
+
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://manngut:qazwsxedcrfv@cluster0.h6puu.mongodb.net/shop',
+    SESSION_SECRET: 'something secret',
+    EMAIL: 'SmetVolodymyr@gmail.com',
+    EMAIL_PASSWORD: '2003Vova2003',
+    EMAIL_FROM: 'cousesApp@shop.com',
+    BASE_URL: 'http://localhost:3000'
 }
